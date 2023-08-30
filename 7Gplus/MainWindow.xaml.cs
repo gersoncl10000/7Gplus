c56ac365d43b216789b688eb163672b49f18d0b4
Gerson Castillo <gersoncl10000@outlook.com>
2023-08-29 17:34:27 -0700

    Actualizar

--- 7Gplus/MainWindow.xaml.cs
+++ 7Gplus/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace _7Gplus
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// 
-    /// 
+    ///
     /// </summary>
     public partial class MainWindow : Window
     {
